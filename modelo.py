e1c9f42d66483f102b31ae31376aa67b243642f9
Joao Victor <falejoaovictor@hotmail.com>
2021-05-11 23:08:35 -0300

    polimorfismo

--- modelo.py
+++ modelo.py
@@ -35,8 +35,15 @@ vingadores.dar_likes()
 vingadores.dar_likes()
 vingadores.dar_likes()
 
+print(f'{vingadores.nome} - {vingadores.duracao} - {vingadores.likes}')
+
 atlanta.dar_likes()
 atlanta.dar_likes()
 
-print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
-print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+print(f'{atlanta.nome} - {atlanta.temporadas} - {atlanta.likes}')
+
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
+    print(f'{programa.nome} - {detalhes} D - {programa.likes}')
