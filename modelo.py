9ffdca2c07326b4adb93882eaf578f41747b551b
Joao Victor <falejoaovictor@hotmail.com>
2021-05-11 23:50:06 -0300

    descomplicando uma lista da herança

--- modelo.py
+++ modelo.py
@@ -39,11 +39,18 @@ class Serie(Programa):
     def __str__(self):
         return f'{self._nome} - {self.ano} - {self.temporadas} - {self._likes} likes'
 
-class Playlist(list):
+class Playlist:
     def __init__(self, nome, programas):
       self.nome = nome
-      super().__init__(programas)
+      self._programas = programas
 
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
@@ -64,9 +71,7 @@ atlanta.dar_likes()
 filmes_e_series = [vingadores, atlanta, tmep]
 playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-print(f'Tamanho do playlist: {len(playlist_fim_de_semana)}')
+print(f'Tamanho do playlist: {len(playlist_fim_de_semana.listagem)}')
 
-for programa in playlist_fim_de_semana:
+for programa in playlist_fim_de_semana.listagem:
    print(programa)
-
-print(f'Tá ou não tá? {demolidor in playlist_fim_de_semana}')
