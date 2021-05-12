63b7593fd3d41ac025fcb853063711d9098db851
Joao Victor <falejoaovictor@hotmail.com>
2021-05-11 23:15:40 -0300

    reduzindo if's com polimorfismo

--- modelo.py
+++ modelo.py
@@ -19,31 +19,31 @@ class Programa:
     def nome(self, nome):
         self._nome = nome
 
+
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
+
+    def imprime(self):
+        print(f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} likes')
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def imprime(self):
+        print(f'{self._nome} - {self.ano} - {self.temporadas} - {self._likes} likes')
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+vingadores.dar_likes()
 atlanta = Serie('atlanta', 2018, 2)
-vingadores.dar_likes()
-vingadores.dar_likes()
-vingadores.dar_likes()
-
-print(f'{vingadores.nome} - {vingadores.duracao} - {vingadores.likes}')
-
 atlanta.dar_likes()
 atlanta.dar_likes()
-
-print(f'{atlanta.nome} - {atlanta.temporadas} - {atlanta.likes}')
 
 filmes_e_series = [vingadores, atlanta]
 
 for programa in filmes_e_series:
-    detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
-    print(f'{programa.nome} - {detalhes} D - {programa.likes}')
+    programa.imprime()
