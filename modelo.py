a34409097f55f4a17c76b3166e05b07f0ab2464e
Joao Victor <falejoaovictor@hotmail.com>
2021-05-12 11:39:56 -0300

    começando a utilizar duck typing - __get_item__

--- modelo.py
+++ modelo.py
@@ -44,6 +44,9 @@ class Playlist:
       self.nome = nome
       self._programas = programas
 
+    def __getitem__(self, item):
+        return self._programas[item]
+
     @property
     def listagem(self):
         return self._programas
@@ -71,7 +74,7 @@ atlanta.dar_likes()
 filmes_e_series = [vingadores, atlanta, tmep]
 playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-print(f'Tamanho do playlist: {len(playlist_fim_de_semana.listagem)}')
+print(f'Tamanho do playlist: {len(playlist_fim_de_semana)}')
 
-for programa in playlist_fim_de_semana.listagem:
+for programa in playlist_fim_de_semana:
    print(programa)
